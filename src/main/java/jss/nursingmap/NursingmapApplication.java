137107a919ecbcfa0da5cd7c469801610f55ab70
jsKim <ewhdtls0@naver.com>
2024-06-14 21:12:33 +0900

    initial commit

---
+++ src/main/java/jss/nursingmap/NursingmapApplication.java
@@ -0,0 +1,13 @@
+package jss.nursingmap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NursingmapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NursingmapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jss/nursingmap/NursingmapApplicationTests.java
@@ -0,0 +1,13 @@
+package jss.nursingmap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NursingmapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
